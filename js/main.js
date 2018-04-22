bcf542742c570e7bf3df6046be5e1392496882ca
ktwery <ktwery3@gmail.com>
2018-04-22 13:11:36 -0500

    add error message if user doesnt submit and order of carrots that is in multiples of 3

--- js/main.js
+++ js/main.js
@@ -14,17 +14,29 @@ checkoutButton.addEventListener('click', function(e){
 		price += parseInt(bananasInput.value)*1.5
 	}
 	if(carrotsInput && carrotsInput.value.length > 0){
-		price += parseInt(carrotsInput.value)*3
+		let wasError = showCarrotErrorMessage()
+		if(!wasError){
+			price += parseInt(carrotsInput.value)*3
+		}
 	}
 	totalCostDiv.innerHTML = '<div>Price: $'+price+' </div>'
 })
 
 carrotsInput.addEventListener('keyup', function(e){	
+	showCarrotErrorMessage()
+})
+
+function showCarrotErrorMessage(){
+	var checkoutButton = document.querySelector('.checkoutButton')
+	var carrotsnonMultipleOf3Error = document.querySelector('.carrotsnonMultipleOf3Error')
 	let numberofCarrots = parseInt(carrotsInput.value)
-		if(numberofCarrots %3 != 0){
-			totalCostDiv.prop( "disabled", true );
-		} else {
-			totalCostDiv.prop( "disabled", false );
-		}
+	if(numberofCarrots %3 != 0){
+		checkoutButton.classList.add("disabled");
+		carrotsnonMultipleOf3Error.style.display = block
+		return true
+	} else {
+		checkoutButton.classList.remove( "disabled")
+		carrotsnonMultipleOf3Error.style.display = none
+		return false
 	}
-)
+}
